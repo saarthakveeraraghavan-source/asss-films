dd2cafddb90d46cf17178119d809434ef1a612f7
saarthakveeraraghavan-source <saarthak.veeraraghavan@gmail.com>
2026-06-02 18:32:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const films = {
+1: "https://drive.google.com/file/d/1LtLruJMzO_DU8dbwopqjO4IgVhKcE41F/view?usp=drive_link",
+
+2: "https://drive.google.com/file/d/1yg5lTeSLx4h_1-xwWDw4TMIRSyyE3kMF/view?usp=sharing",
+
+3: "https://drive.google.com/file/d/1CSXmlnbQLzW8m9Tv0SQT0ORvwuk8773Q/view?usp=sharing",
+
+4: "https://drive.google.com/file/d/10YrxNXyqHX-MVRGDAWNl42XFRQYyFBYp/view?usp=sharing"
+
+};
+
+function openFilm(id){
+window.open(films[id], "_blank");
+}
+
+const observer = new IntersectionObserver(entries => {
+
+
+entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+        entry.target.classList.add("show");
+    }
+
+});
+
+},{
+threshold: 0.15
+});
+
+document.querySelectorAll(".film-card").forEach(card => {
+observer.observe(card);
+});
